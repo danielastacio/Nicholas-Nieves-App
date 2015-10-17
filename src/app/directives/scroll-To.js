1ea3016c8ace04734f4430466fd84c18cb96b06b
danielastacio <danielastacio91@gmail.com>
2015-10-17 17:37:41 -0400

    Fixed bug thatmade scrolling not work when a view was changed

--- src/app/directives/scroll-To.js
+++ src/app/directives/scroll-To.js
@@ -3,58 +3,53 @@
         .module('nick.app.directives')
         .directive('scrollTo', ScrollTo);
 
-    ScrollTo.$inject = ['$document','$window'];
+    ScrollTo.$inject = ['$document','$window', '$timeout'];
 
-    function ScrollTo($document, $window) {
+    function ScrollTo($document, $window, $timeout) {
         return {
             restrict: 'A',
             link: function (scope, element, attr) {
 
-            $document.ready(function () {
+                element.on('click', function () {
 
-                var valueOfAttr = attr.scrollTo;
-                var arrayOfAttr = attr.scrollTo.split(',');
-                var allElemArray = [];
-                var allElemNodes = [];
-                var allElemIds = [];
-                var allElemClasses = [];
-                var target;
-                
-                    element.on('click', function () {
+                    var valueOfAttr = attr.scrollTo;
+                    var arrayOfAttr = attr.scrollTo.split(',');
+                    var allElemArray = [];
+                    var allElemNodes = [];
+                    var allElemIds = [];
+                    var allElemClasses = [];
+                    var target;
 
-                        allElemArray.push(document.body.querySelectorAll('*'));
+                    allElemArray.push(document.body.querySelectorAll('*'));
 
-                        for (i = 0; i < allElemArray[0].length; i++) {
+                    for (i = 0; i < allElemArray[0].length; i++) {
 
-                            allElemNodes.push(allElemArray[0][i]);
-                            allElemIds.push(allElemArray[0][i].id);
-                            allElemClasses.push(allElemArray[0][i].className);
+                        allElemNodes.push(allElemArray[0][i]);
+                        allElemIds.push(allElemArray[0][i].id);
+                        allElemClasses.push(allElemArray[0][i].className);
 
-                            if (allElemClasses[i].indexOf(valueOfAttr) > -1) {
+                        if (allElemClasses[i].indexOf(valueOfAttr) > -1) {
 
-                               target = allElemNodes[i];
+                           target = allElemNodes[i];
 
-                               console.log(target.offsetTop);
+                           $('html, body').animate({
+                            scrollTop: target.offsetTop
+                           });
 
-                               $('html, body').animate({
-                                scrollTop: target.offsetTop
-                               });
+                        } // End of if statement
 
-                            } // End of if statement
+                        if (allElemIds[i].indexOf(valueOfAttr) > -1) {
 
-                            if (allElemIds[i].indexOf(valueOfAttr) > -1) {
+                           target = allElemNodes[i];
 
-                               target = allElemNodes[i];
+                           $('html, body').animate({
+                            scrollTop: target.offsetTop
+                           });
 
-                               $('html, body').animate({
-                                scrollTop: target.offsetTop
-                               });
+                        } // End of if statement
+                    } // End of for loop
 
-                            } // End of if statement
-                        } // End of for loop
-                    }); // End of element click function
-
-                }); // end of document ready
+                }); // End of element click function
             } // end of link function
         }; // end of return
     }
